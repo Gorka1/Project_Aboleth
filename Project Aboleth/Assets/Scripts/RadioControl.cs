dabe2775ac257ceda68ed120f6143d0156b6c402
Gorka1 <javelasquez2001@gmail.com>
2020-07-16 19:56:12 -0400

    added the radio and music

---
+++ Project Aboleth/Assets/Scripts/RadioControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Audio;
+using UnityEngine;
+
+public class RadioControl : MonoBehaviour
+{
+    public AudioClip[] audioArray;
+    public List<AudioClip> audioList = new List<AudioClip>();
+    private AudioSource source;
+
+    void refillList()
+    {
+        foreach (AudioClip a in audioArray)
+        {
+            audioList.Add(a);
+        }
+    }
+
+    AudioClip getNewSong()
+    {
+        if (audioList.Count == 0)
+        {
+            refillList();
+        }
+        int tempNum = Random.Range(0, audioList.Count);
+        AudioClip temp = audioList[tempNum];
+        audioList.RemoveAt(tempNum);
+        return temp;
+    }
+
+    void Awake()
+    {
+        source = gameObject.GetComponent<AudioSource>();
+        refillList();
+        getNewSong();
+    }
+
+    private void Update()
+    {
+        if (source.isPlaying == false)
+        {
+            AudioClip temp = getNewSong();
+            source.clip = temp;
+            source.Play();
+        }
+    }
+}
